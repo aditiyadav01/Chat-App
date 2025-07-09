cb37bcdc48f96e1ae52d4cb4eaf7922026b4a78c
aditiyadav01 <yadavaditi0501@gmail.com>
2025-07-09 20:27:57 +0530

    commit this file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import ChatProvider from "./Context/ChatProvider.jsx";
+import ChatProvider from "./context/ChatProvider";
 
 createRoot(document.getElementById("root")).render(
   <Router>
